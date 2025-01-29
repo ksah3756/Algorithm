4f420d8aa7e33dd40c24c568145673935435c292
ksah3756 <ksah3756@gmail.com>
2025-01-29 23:42:22 +0900

    [Gold IV] Title: 합이 0, Time: 144 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.cc
+++ 백준/Gold/3151. 합이 0/합이 0.cc
@@ -4,6 +4,8 @@ using namespace std;
 typedef long long ll;
 int n;
 int arr[10001];
+ll ans;
+void solve(int,int);
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -14,15 +16,42 @@ int main() {
     for(int i = 0; i < n; i++) cin >> arr[i];
     sort(arr, arr+n);
 
-    ll sum, cnt, ans = 0;
-    for(int i = 0; i < n-2; i++) {
-        for(int j = i+1; j < n-1; j++) {
-            sum = arr[i] + arr[j];
-            cnt = upper_bound(arr+j+1, arr+n, -sum) - lower_bound(arr+j+1, arr+n, -sum);
-            ans += cnt;
-        }
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > 0) break;
+        solve(i, -arr[i]);
     }
     
     cout << ans << '\n';
     return 0;
+}
+
+
+void solve(int idx, int target) {
+    int l = idx+1, r = n-1;
+    while(l < r) {
+        int sum = arr[l] + arr[r];
+        if(sum < target) l++;
+        else if(sum > target) r--;
+        else {
+            if(arr[l] == arr[r]) {
+                int cnt = r-l+1;
+                ans += cnt * (cnt-1) / 2;
+                break;
+            } else {
+                int l_cnt = 1;
+                while(arr[l] == arr[l+1]) {
+                    l_cnt++;
+                    l++;
+                }
+                l++;
+                int r_cnt = 1;
+                while(arr[r-1] == arr[r]) {
+                    r_cnt++;
+                    r--;
+                }
+                r--;
+                ans += l_cnt * r_cnt;
+            }  
+        }
+    }
 }
