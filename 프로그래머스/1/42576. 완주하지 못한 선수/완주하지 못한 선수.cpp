c5f20a48673566770bccbed797f14b923a6423a0
ksah3756 <ksah3756@gmail.com>
2025-03-20 14:52:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 42.42 ms, Memory: 32.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    unordered_map<string, int> um;
+    for(string name: participant) {
+        um[name]++;
+    }
+    for(string name: completion) {
+        um[name]--;
+    }
+    
+    for(auto m: um) {
+        if(m.second != 0) answer = m.first;
+    }
+    return answer;
+}
