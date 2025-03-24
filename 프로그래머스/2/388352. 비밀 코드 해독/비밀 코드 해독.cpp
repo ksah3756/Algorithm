92b9fb3dab02bd7cf811c0b7d6fa38dcb39336eb
ksah3756 <ksah3756@gmail.com>
2025-03-24 13:34:12 +0900

    [level 2] Title: 비밀 코드 해독, Time: 2.49 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.cpp
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.cpp
@@ -1,5 +1,4 @@
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 vector<int> codes;
@@ -7,42 +6,33 @@ int ansCnt, N;
 
 //11:48
 // 30C5해서 시스템 응답이랑 일치하는지 확인
-void solve(const vector<vector<int>> &q, const vector<int>& ans, int cur) {
-    if(codes.size() == 5) {
-        bool isPossible = 1;
+int solution(int n, vector<vector<int>> q, vector<int> ans)  {
+    int answer = 0;
+    vector<int> codes(n);
+    
+    for(int i = 0; i < 5; i++) {
+        codes[n-1-i] = 1;
+    }
+    
+    bool isPossible;
+    do {
         for(int i = 0; i < q.size(); i++) {
-            int i1 = 0, i2 = 0, cnt = 0;
-            while(i1 < 5 && i2 < 5) {
-                if(q[i][i1] == codes[i2]) {
-                    cnt++;
-                    i1++; i2++;
-                } else if(q[i][i1] > codes[i2]) {
-                    i2++;
-                } else {
-                    i1++;
-                }
+            int cnt = 0;
+            isPossible = 1;
+            for(int j = 0; j < 5; j++) {
+                int idx = q[i][j]-1;
+                cnt += codes[idx];
             }
             if(cnt != ans[i]) {
                 isPossible = 0;
                 break;
             }
         }
-        if(isPossible) ansCnt++;
-    }
+        if(isPossible) answer++;
+    } while(next_permutation(codes.begin(), codes.end()));
     
-    for(int i = cur; i <= N; i++) {
-        codes.push_back(i);
-        solve(q, ans, i+1);
-        codes.pop_back();
-    }
-}
-
-int solution(int n, vector<vector<int>> q, vector<int> ans) {
-    int answer = 0;
-    N = n;
-    solve(q, ans, 1);
-    answer = ansCnt;
     return answer;
 }
+
 
 
