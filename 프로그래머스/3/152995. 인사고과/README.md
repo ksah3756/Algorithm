39d15800b6855cac97bc43ce26626ab99c164038
ksah3756 <ksah3756@gmail.com>
2025-04-06 16:48:24 +0900

    [level 3] Title: 인사고과, Time: 26.02 ms, Memory: 21.4 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/README.md
+++ 프로그래머스/3/152995. 인사고과/README.md
@@ -1,10 +1,10 @@
 # [level 3] 인사고과 - 152995 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152995#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152995) 
 
 ### 성능 요약
 
-메모리: 25.1 MB, 시간: 30.10 ms
+메모리: 21.4 MB, 시간: 26.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 06일 16:02:31
+2025년 04월 06일 16:48:22
 
 ### 문제 설명
 
--- 프로그래머스/3/152995. 인사고과/인사고과.cpp
+++ 프로그래머스/3/152995. 인사고과/인사고과.cpp
@@ -1,59 +1,32 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-typedef pair<int,int> pii;
-// 3:37
-// {a,b}를 정렬해서 a 오름차순 순으로 하나씩 확인해서 스택에 넣는데, top보다 큰 수가 나타난다면 top이 크거나 같아질때 까지 pop하고 해당 수를 push 
-// 이 스택에 남은 score에 대해서만 합을 구해서 map에 기록
+
 int solution(vector<vector<int>> scores) {
     int answer = 0;
     
-    vector<pii> score;
-    for(const vector<int>& v : scores) {
-        score.push_back({v[0],v[1]});
-    }
-    
-    auto cmp = [](const pii& p1, const pii& p2) -> bool {
-        if(p1.first == p2.first) return p1.second > p2.second;
-        return p1.first < p2.first;
+    auto cmp = [](const vector<int>& v1, const vector<int>& v2) -> bool {
+      if(v1[0] == v2[0]) return v1[1] < v2[1];
+        return v1[0] > v2[0];
     };
     
-    sort(score.begin(), score.end(), cmp);
+    // 어짜피 완호 점수보다 높은 사람들이 몇명인지 세는 문제인거니까 
+    int targetA = scores[0][0], targetB = scores[0][1];
+    int targetScore = targetA + targetB;
     
-    stack<pii> stk;
+    sort(scores.begin(), scores.end(), cmp);
     
-    // 이렇게 하면 2,2 2,3 들어왔을 때 2,2 아웃되네
-    // 근데 2,5 2,1 이렇게 있는데 3,2 들어오면 2,1은 아웃되지만 2,5는 아웃되면 안됨
-    for(int i = 0; i < score.size(); i++) {
-        while(!stk.empty() && stk.top().first < score[i].first && stk.top().second < score[i].second) stk.pop();
-        stk.push(score[i]);
-    }
-    
-    bool isPossible = 0;
-    map<int,int> mp;
-    while(!stk.empty()) {
-        int a = stk.top().first;
-        int b = stk.top().second;
-        // cout << a << ' ' << b << '\n';
-        if(a == scores[0][0] && b == scores[0][1]) isPossible = 1;
-        mp[a+b]++;
-        stk.pop();
-    }
-    
-    if(!isPossible) return -1;
-    
-    int ansSum = scores[0][0] + scores[0][1];
-    int rank = 1;
-    for(auto iter = --mp.end(); ; iter--) {
-        if(iter->first == ansSum) {
-            break;
+    int maxB = -1;
+    for(int i = 0; i < scores.size(); i++) {
+        if(scores[i][1] < maxB) continue; // 인센티브 지급 X
+        if(targetA < scores[i][0] && targetB < scores[i][1]) return -1;
+        
+        if(scores[i][0] + scores[i][1] > targetScore) {
+            answer++;
         }
-        int cnt = iter->second;
-        rank += cnt;
-        if(iter == mp.begin()) break;
+        maxB = scores[i][1];
     }
     
-    isPossible ? answer = rank : answer = -1;
-    
+    answer++;
     return answer;
 }
