0e6807cfe27aee44d2d00734d2160220e323039b
ksah3756 <ksah3756@gmail.com>
2025-04-07 22:22:42 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/README.md
+++ 프로그래머스/2/148653. 마법의 엘리베이터/README.md
@@ -1,10 +1,10 @@
 # [level 2] 마법의 엘리베이터 - 148653 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.01 ms
+메모리: 3.64 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 07일 22:16:18
+2025년 04월 07일 22:22:41
 
 ### 문제 설명
 
--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
@@ -4,23 +4,19 @@
 
 // 10:00
 // 뒷자리에서부터 0으로 만들어야 하는데 빼기 또는 더하기
+// 마지막 제일 큰 자리는 5 이하면 x, 5 초과면 1
 using namespace std;
 
 int ans = 1e+8+1;
 void solve(int cur, int cnt) {
-    if(cur == 0) {
-        ans = min(ans, cnt);
+    if(cur < 10) {
+        int tmp = min(cnt+cur, cnt+11-cur);
+        ans = min(ans, tmp);
         return;
     }
     int x = cur % 10;
     solve(cur/10, cnt+x);
-    if(cur / 10 != 0) {
-        solve(cur/10 + 1, cnt+10-x);
-    } else {
-        if(x >= 6)
-            solve(cur / 10 + 1, cnt+10-x);
-    }
-        
+    solve(cur/10 + 1, cnt+10-x);
 }
 
 int solution(int storey) {
