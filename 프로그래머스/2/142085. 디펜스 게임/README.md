f7410e452b543227f0aaeec3c58202aaf8bf09a6
ksah3756 <ksah3756@gmail.com>
2025-04-08 11:46:44 +0900

    [level 2] Title: 디펜스 게임, Time: 22.64 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/README.md
+++ 프로그래머스/2/142085. 디펜스 게임/README.md
@@ -1,10 +1,10 @@
 # [level 2] 디펜스 게임 - 142085 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085) 
 
 ### 성능 요약
 
-메모리: 41.4 MB, 시간: 798.21 ms
+메모리: 38.6 MB, 시간: 22.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 08일 10:57:50
+2025년 04월 08일 11:46:43
 
 ### 문제 설명
 
--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.cpp
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.cpp
@@ -1,37 +1,28 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-typedef long long ll;
-// 10:36
-// 몇 개의 라운드를 해결할 수 있을지 이분 탐색으로 정하고 가능한지 확인해서 가능하면 오른쪽, 불가능하면 왼쪽
-// 가능한지 확인하는건 내림차순으로 정렬 후 앞에서부터 k개 없애서 합이 n보다 작거나 같은지 확인
-int solution(int n, int k, vector<int> enemy) {
-    int answer = 0;
-    
-    auto checkIfPossible = [&](int lastIdx) -> bool {
-        vector<int> v(lastIdx+1);
-        ll sum = 0;
-        for(int i = 0; i <= lastIdx; i++) {
-            v[i] = enemy[i];
-            sum += enemy[i];
-        }
 
-        if(sum <= n) return true;
+int solution(int n, int k, vector<int> enemy) {
+    int answer = -1;
+    
+    priority_queue<int> pq;
+    for(int i = 0; i < enemy.size(); i++) {
+        n -= enemy[i];
+        pq.push(enemy[i]);
         
-        sort(v.begin(), v.end(), greater<int>());
-        for(int i = 0; i < k && i <= lastIdx; i++) sum -= v[i];
-        if(sum <= n) return true;
-        return false;
-    };
-    
-    int l = 0, r = enemy.size()-1, mid;
-    
-    while(l < r) {
-        mid = (l+r+1) / 2;
-        if(checkIfPossible(mid)) l = mid;
-        else r = mid-1;
+        if(n < 0) {
+            if(k) {
+                int x = pq.top(); pq.pop();
+                n += x;
+                k--;
+            }
+            else {
+                answer = i;
+                break;
+            }
+        }
     }
     
-    answer = l+1;
-    return answer;     
+    if(answer == -1) answer = enemy.size();
+    return answer;
 }
