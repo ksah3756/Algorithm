823f9fc378286202a6e12c21339915ecd31319aa
ksah3756 <ksah3756@gmail.com>
2025-04-24 11:18:39 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 0.52 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.cpp
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.cpp
@@ -1,49 +1,66 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
+// 각 블록에 대해 내 밑에 빈 공간의 개수가 몇개인지 파악하면 그만큼 내리면 됨
+// 중복되지 않게 하기 위해선 밑에서부터 위로 순회하면서 밑으로 이동시키기
 bool isDeleted[31][31];
+int downCount[31][31];
 
 int solution(int m, int n, vector<string> board) {
     int answer = 0;
     
+    vector<int> height(n, m);
+    
     while(1) {
         bool isEnd = 1;
-        for(int i = 0; i < m-1; i++) {
-            for(int j = 0; j < n-1; j++) {
-                if(board[i][j] != ' ' &&
-                   board[i][j] == board[i+1][j] && 
-                   board[i][j] == board[i][j+1] &&
-                   board[i][j] == board[i+1][j+1]) {
-                    isEnd = 0;
-                    isDeleted[i][j] = 1;
-                    isDeleted[i+1][j] = 1;
-                    isDeleted[i][j+1] = 1;
-                    isDeleted[i+1][j+1] = 1;
+        for(int j = 0; j < n-1; j++) {
+            int h = height[j];
+            for(int i = 0; i < h-1; i++) {
+                if(max({board[m-2-i][j], board[m-2-i][j+1], board[m-1-i][j], board[m-1-i][j+1]})
+                     == min({board[m-2-i][j], board[m-2-i][j+1], board[m-1-i][j], board[m-1-i][j+1]})) {
+                        isEnd = 0;
+                         isDeleted[m-2-i][j] = 1; 
+                         isDeleted[m-2-i][j+1] = 1;
+                         isDeleted[m-1-i][j] = 1;
+                         isDeleted[m-1-i][j+1] = 1;
                 }
             }
         }
         
         if(isEnd) break;
-        
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(isDeleted[i][j]) {
-                    board[i][j] = ' ';
+
+        for(int j = 0; j < n; j++) {
+            int idx = m-1, cnt = 0;
+            int h = height[j];
+
+            while(m-h <= idx && !isDeleted[idx--][j]);
+            idx++;
+            while(m-h <= idx) {
+                while(m-h <= idx && isDeleted[idx--][j]) {
+                    cnt++;
                     answer++;
                 }
+                idx++;
+                while(m-h <= idx && !isDeleted[idx--][j]) {
+                    downCount[idx][j] = cnt;
+                }
+                idx++;
             }
-        }
-        
-        for(int j = 0; j < n; j++) {
-            int h = m-1; // 블록이 없는 시작위치
-            for(int i = m-1; i >= 0; i--) {
-                if(board[i][j] == ' ') continue;
-                swap(board[h--][j], board[i][j]);
+
+            for(int i = m-1; i >= m-h; i--) {
+                int down = downCount[i][j];
+                if(down) {
+                    cout << j << ' ' << i << ' ' << down << '\n';
+                    board[i+down][j] = board[i][j];
+                }
             }
+            height[j] -= cnt; 
         }
         
         memset(isDeleted, 0, sizeof(isDeleted));
+        memset(downCount, 0, sizeof(downCount));
     }
-    
+            
     return answer;
 }
