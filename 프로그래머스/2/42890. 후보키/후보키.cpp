270dae23d9273e51a9882141591f087b03ff0af1
ksah3756 <ksah3756@gmail.com>
2025-04-24 18:19:17 +0900

    [level 2] Title: 후보키, Time: 1.16 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.cpp
+++ 프로그래머스/2/42890. 후보키/후보키.cpp
@@ -1,63 +1,38 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-// 각 칼럼별로 겹치는게 있으면 다른 칼럼들을 쭉 비교해서 서로 다른 칼럼이 있으면 그걸 포함하면 후보키
-// 아니면 8C1~8C8 까지 쭉 키를 만들고 이게 유일성을 가지면 일단 저장하고, 나중에 최소성을 만족하지 않으면 제외
-// 이걸 후보키가 되는 칼럼을 비트마스크로 표현하고 or 연산했을 때 자기 자신이 나오면 자신보다 더 작은 크기의 후보키가 있는 것이므로 제외?
-int cSize, rSize;
-vector<int> candidates;
 
 int solution(vector<vector<string>> relation) {
     int answer = 0;
-    cSize = relation[0].size();
-    rSize = relation.size();
+    int rSize = relation.size();
+    int cSize = relation[0].size();
     
-    function<bool(int)> checkIfKey;
+    vector<int> candidates;
     
-    checkIfKey = [&](int state) -> bool {
+    auto checkIfMinimum = [&](int comb) -> bool {
+        // 나보다 작은 수들만 candidates에 포함되므로 이전 키와 내가 and 연산했을 때 해당 키면 나는 최소성 만족 X
+        for(int i : candidates) {
+            if((i & comb) == i) return false;
+        }
+        return true;
+    };
+    
+    int all = (1 << cSize) - 1;
+    for(int comb = 1; comb <= all; comb++) {
         unordered_set<string> set;
         for(int i = 0; i < rSize; i++) {
             string s = "";
             for(int j = 0; j < cSize; j++) {
-                if(state & (1<<j)) {
+                if(comb & (1<<j)) {
                     s += relation[i][j];
                 }
             }
             set.insert(s);
         }
-    
-        if(set.size() == rSize) return true;
-        return false;
-    };
-    
-    function<void(int,int)> dfs;
-    dfs = [&](int state, int idx) -> void {
-        if(idx == cSize) {
-            if(checkIfKey(state))
-                candidates.push_back(state);
-            return;
+        if(set.size() == rSize && checkIfMinimum(comb)) {
+            candidates.push_back(comb);
+            answer++;
         }
-
-        dfs(state | (1 << idx), idx+1);
-        dfs(state, idx+1);
-    };
-    
-    dfs(0,0);
-    
-    for(int i = 0; i < candidates.size(); i++) {
-        int c1 = candidates[i];
-        bool isPossible = 1;
-        for(int j = 0; j < candidates.size(); j++) {
-            if(i == j) continue;
-            int c2 = candidates[j];
-            if((c1 | c2) == c1) {
-                isPossible = 0;
-                break;
-            }
-        }
-        if(isPossible) answer++;
     }
-    
     return answer;
 }
-
