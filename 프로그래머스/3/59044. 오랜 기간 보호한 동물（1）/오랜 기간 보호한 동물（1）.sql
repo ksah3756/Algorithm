54792f6a3736c3d674af83b77431362727fa73db
ksah3756 <ksah3756@gmail.com>
2025-05-05 18:26:37 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,14 @@
+-- 코드를 입력하세요
+# select ai.name, ao.name, ai.datetime, ao.datetime
+# from ANIMAL_INS ai
+# left join ANIMAL_OUTS ao
+# on ai.name = ao.name
+
+
+SELECT ai.name, ai.datetime
+from ANIMAL_INS ai
+left join ANIMAL_OUTS ao
+on ai.animal_id = ao.animal_id
+where ao.name is null and ai.name is not null
+order by ai.datetime
+limit 3
