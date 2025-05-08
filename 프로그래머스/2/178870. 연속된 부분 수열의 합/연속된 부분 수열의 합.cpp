c7e84af2c06f7768af711002a261376ed1788db2
ksah3756 <ksah3756@gmail.com>
2025-05-08 11:11:18 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 7.12 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
@@ -1,34 +1,29 @@
-#include <string>
-#include <vector>
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
-typedef pair<int,int> pii;
-// 3:20
-// 가장 짧은 부분수열, 여러개면 맨 앞에
-// 투포인터같은데
+
 vector<int> solution(vector<int> sequence, int k) {
-    vector<int> answer;
-    answer.resize(2, -1);
+    vector<int> answer = {-1,-1};
     
-    vector<int> sum(sequence.size()+1, 0);
-    for(int i = 1; i <= sequence.size(); i++) {
-        sum[i] = sequence[i-1] + sum[i-1];
-    }
+    int size = sequence.size();
     
-    int s, e;
-    s = e = 1;
-    while(s <= e && e <= sequence.size()) {
-        int tmp = sum[e] - sum[s-1];
-        if(tmp < k) e++;
-        else if(tmp > k) s++;
-        else {
-            if((answer[0] == -1 && answer[1] == -1) || (answer[1]-answer[0] > e-s)) {
-                answer[0] = s-1;
-                answer[1] = e-1;
+    int s = 0, e = 0;
+    int sum = sequence[s];
+    while(e < size) {
+        while(e < size && sum < k) sum += sequence[++e];
+        
+        if(e == size) break;
+        
+        if(sum == k) {
+            if(answer[0] == -1 || answer[1]-answer[0] > e-s) {
+                answer = {s,e};
             }
-            e++;
+            if(e+1 < size) sum += sequence[++e];
+            else break;
         }
+        
+        while(s < e && sum > k) sum -= sequence[s++];
+        if(s == e && sum > k) break;
     }
     return answer;
 }
